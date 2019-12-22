b46c6a12c153eec4ce5be1c8c8fe552b3f20e511
root <root@localhost.localdomain>
2019-12-22 00:16:12 -0500

    added ex32.py, for loops andl list

---
+++ ex32.py
@@ -0,0 +1,26 @@
+the_count = [1,2,3,4,5]
+
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+
+change = [1, 'coins', 2 , 'dimes', 3, 'quarters']
+
+for number in the_count:
+	print(f"This is count{number}")
+
+for fruit in fruits:
+	print (f"A fruit of type: {fruit}")
+
+for i in change:
+	print (f"I got {i}")
+
+# Lets build a list, first start with an empty one
+elements = []
+
+for i in range(0, 6):
+	print(f"Adding {i} to the list.")
+	elements.append(i)
+
+for i in elements:
+	print (f"Element was: {i}")
+
+
