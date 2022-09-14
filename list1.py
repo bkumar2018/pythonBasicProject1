3d3295c8e409819f11fc55a1eecb1d90ecba47aa
bkumar2018 <birender.cdac@gmail.com>
2022-09-14 13:14:07 +0530

    updated list1

--- list1.py
+++ list1.py
@@ -9,3 +9,26 @@ list.append(100)
 list.append([1,21,2,3])
 print(list)
 
+
+squares=[1,4,8,15,25]
+print(squares)
+print(squares[0])
+print(squares[3])
+print(squares[-1])
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters)
+print(letters[2:5])
+#replace some values
+letters[2:5] = ['C', 'D', 'E']
+print(letters)
+#now remove them
+letters[2:5] = []
+print(letters)
+#get length of list
+print(len(letters))
+
+a=['a', 'b', 'c']
+b=[1, 2, 3]
+x=[a,b]
+print(x[0][1])
